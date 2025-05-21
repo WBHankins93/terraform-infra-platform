65dfeeef5a72f9406bd96f40a128bf0b44e1f2d7
WBHankins93@gmail.com Ben Hankins <benhankins93@gmail.com>
2025-05-21 14:36:29 -0500

    Add IGW, EIP, and NAT gateway with clear resource names

---
+++ modules/vpc/nat.tf
@@ -0,0 +1,28 @@
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat_eip" {
+  vpc = true
+
+  tags = {
+    Name = "${var.name}-nat-eip"
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public[0].id
+  depends_on    = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "${var.name}-natgw"
+  }
+}
