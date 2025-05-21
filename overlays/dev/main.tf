b8c69078e21bca43e6b077e26c00299350ed1622
Ben Hankins <benhankins93@gmail.com>
2025-05-21 15:34:22 -0500

    Add minimal EKS cluster and IAM role module with integration to dev overlay

--- overlays/dev/main.tf
+++ overlays/dev/main.tf
@@ -3,3 +3,9 @@ module "vpc" {
   name       = var.name
   cidr_block = var.cidr_block
 }
+
+module "eks" {
+  source     = "../../modules/eks"
+  name       = var.name
+  subnet_ids = module.vpc.private_subnet_ids
+}
