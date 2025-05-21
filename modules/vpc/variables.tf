009a3fa759d800e96e1c3bb8b0da0d40b43a19fe
Ben Hankins <benhankins93@gmail.com>
2025-05-21 12:32:09 -0500

    Define cidr_block and name variables for VPC module

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  type        = string
+  description = "The CIDR block to assign to the VPC"
+}
+
+variable "name" {
+  type        = string
+  description = "A name to tag the VPC with"
+}
