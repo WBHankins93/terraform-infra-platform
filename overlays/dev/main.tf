708a2ff72bc13cb7339704174b10120081b5e7ab
Ben Hankins <benhankins93@gmail.com>
2025-05-21 16:16:23 -0500

    helm add-ons

--- overlays/dev/main.tf
+++ overlays/dev/main.tf
@@ -16,3 +16,13 @@ module "eks" {
   subnet_ids        = module.vpc.private_subnet_ids
   node_instance_type = "t3.medium"
 }
+
+module "helm_addons" {
+  source              = "../../modules/helm-addons"
+  cluster_name        = module.eks.cluster_name
+  cluster_endpoint    = module.eks.cluster_endpoint
+  cluster_certificate = module.eks.cluster_certificate
+  kube_token          = var.kube_token
+  aws_region          = "us-east-1"
+  depends_on_cluster  = module.eks
+}
