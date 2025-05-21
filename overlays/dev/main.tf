904104526f96936446408bd017b6b40e77aa3fd1
WBHankins93@gmail.com Ben Hankins <benhankins93@gmail.com>
2025-05-21 16:02:05 -0500

    add node_group, shell for helm-addons

--- overlays/dev/main.tf
+++ overlays/dev/main.tf
@@ -9,3 +9,10 @@ module "eks" {
   name       = var.name
   subnet_ids = module.vpc.private_subnet_ids
 }
+
+module "eks" {
+  source            = "../../modules/eks"
+  name              = var.name
+  subnet_ids        = module.vpc.private_subnet_ids
+  node_instance_type = "t3.medium"
+}
