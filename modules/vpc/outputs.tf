234d85ed7e89d56f0abbec0644620c57a3882667
Ben Hankins <benhankins93@gmail.com>
2025-05-22 16:33:52 -0500

    Add private route table and NAT association for secure outbound traffic

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -30,3 +30,13 @@ output "private_route_table_id" {
   value       = aws_route_table.private.id
   description = "The ID of the private route table"
 }
+
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  description = "ID of the private route table"
+  value       = aws_route_table.private.id
+}
