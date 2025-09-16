655ada7626cd51f7bdef53ddec30e54064fcf60b
Ben Hankins <WBHankins93@gmail.com>
2025-09-15 21:10:05 -0500

    updating route_tables and route_table outputs

--- modules/vpc/route_tables.tf
+++ modules/vpc/route_tables.tf
@@ -1,9 +1,9 @@
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
